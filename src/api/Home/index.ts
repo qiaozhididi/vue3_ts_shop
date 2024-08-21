71a0ac9750314eeccdd92023ac9c329d6be52dae
qiaozhididi <1343013603@qq.com>
2024-08-21 20:35:33 +0800

    前端解决跨域

--- src/api/Home/index.ts
+++ src/api/Home/index.ts
@@ -3,7 +3,7 @@ import axios from "../../utils/request";
 // banner轮播图
 export function getBanners() {
   return axios({
-    url: "http://localhost:3010/api/banner",
+    url: "/api/api/banner",
     method: "get",
   });
 }
