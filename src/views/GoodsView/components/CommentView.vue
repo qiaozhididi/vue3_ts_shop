b8cdcb4bb5f09cae522c4c909e407c79f6ed7f2b
qiaozhididi <1343013603@qq.com>
2024-08-22 22:22:06 +0800

    fix: 新增商品详情页底部footer显示错误问题

--- src/views/GoodsView/components/CommentView.vue
+++ src/views/GoodsView/components/CommentView.vue
@@ -26,15 +26,7 @@
       </div>
     </div>
   </div>
-  <div class="footer">
-    <van-action-bar>
-      <van-action-bar-icon icon="chat-o" text="客服" />
-      <van-action-bar-icon icon="cart-o" text="购物车" />
-      <van-action-bar-icon icon="shop-o" text="店铺" />
-      <van-action-bar-button type="warning" text="加入购物车" />
-      <van-action-bar-button type="danger" text="立即购买" @click="onBuy" />
-    </van-action-bar>
-  </div>
+
 </template>
 <script setup lang="ts">
 import { ref, reactive } from "vue";
