c4e58428c9ae38803d6d211e70ce523bc79d7404
qiaozhididi <1343013603@qq.com>
2024-08-24 14:57:49 +0800

    feat: 商城页面新增shopNav

--- src/views/ShopView/ShopView.vue
+++ src/views/ShopView/ShopView.vue
@@ -1,11 +1,13 @@
 <template>
   <TopNavComponent />
   <SwiperComponent :images="banners" />
+  <ShopNavView />
 </template>
 <script setup lang="ts">
 import { ref, reactive, onMounted } from "vue";
 import TopNavComponent from "@/components/TopNavComponent.vue";
 import SwiperComponent from "@/components/SwiperComponent.vue";
+import ShopNavView from "./ShopNavView.vue";
 import { getBanners } from "@/api/Home/index";
 
 //Banner
